ddebe32580db95f35d7c4797c568dbfdc76c4b36
bustawin <xavier@bustawin.com>
2024-05-28 08:00:21 +0200

    Load icons from path directly to avoid Vite bug

--- src/ui/components/icon/Icon.tsx
+++ src/ui/components/icon/Icon.tsx
@@ -1,19 +1,25 @@
-import bsIcons from 'bootstrap-icons/bootstrap-icons.svg'
 import * as ut from '@jutils/ui/reactUtils'
 
 interface IconProps {
   className?: ut.ClassName
   name: string
   button?: boolean
+  // Can't import icons directly because of a vite bug https://github.com/vitejs/vite/issues/4454
+  path?: string
 }
 
-export default function Icon({ name, button = false, className }: IconProps) {
+export default function Icon({
+  name,
+  button = false,
+  className,
+  path = '/icons.svg',
+}: IconProps) {
   return (
     <svg
       className={ut.cls('bi', 'icon', button && 'icon--button', className)}
       fill="currentColor"
     >
-      <use xlinkHref={`${bsIcons}#${name}`} />
+      <use xlinkHref={`${path}#${name}`} />
     </svg>
   )
 }
